9f2b1f7c3c99d48cc128c8cd41ec310573e1cdfb
George Zalepa <george.zalepa@gmail.com>
2025-10-02 16:19:32 -0400

    change active_admin status field to select

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -12,4 +12,19 @@ ActiveAdmin.register Issue do
   filter :created_at
   filter :updated_at
 
+  # Customize the form
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :category, as: :select, collection: Category.active.leaf_nodes.ordered
+      f.input :status, as: :select, collection: Issue.statuses.keys.map { |k| [k.titleize, k] }
+      f.input :comment
+      f.input :street_address
+      f.input :latitude
+      f.input :longitude
+      f.input :photo, as: :file
+    end
+    f.actions
+  end
+
 end
